a11902fcbeb6599e488c4ab8661f285fa150e3a6
Eraj <eracialam@gmail.com>
2024-11-06 00:12:37 +0500

    feat: audio processing "result" returning fixed

--- api/src/services/ModelService.py
+++ api/src/services/ModelService.py
@@ -14,4 +14,4 @@ class ModelService:
     def get_text_from_mp3(self, mp3_path: str):
         model = whisper.load_model("small")
         result = model.transcribe(mp3_path)
-        return result
+        return result["text"]
